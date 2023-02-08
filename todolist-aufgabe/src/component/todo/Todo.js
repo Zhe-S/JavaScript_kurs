5a85f60b52f5a6ef7117e57b5db80ebe1fbcd637
SL <sunli@SLdeMacBook-Pro.fritz.box>
2023-02-08 15:07:12 +0100

    todolist-anfang

---
+++ todolist-aufgabe/src/component/todo/Todo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Todo = () => {
+    return (
+        <div>
+            <input type="text" placeholder="was zu tun"/>
+            <button> + </button>
+            <ul>
+                <li>pizza kaufen</li>
+                <li>Dally</li>
+                <li>TOdo-List machen</li>
+            </ul>
+        </div>
+    )
+}
+export default Todo
